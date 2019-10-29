500ff544b7940063b739182c88dda9cd95c0e3bc
bky373 <49539592+bky373@users.noreply.github.com>
2019-10-29 11:47:15 +0900

    1204. [S/W 문제해결 기본] 1일차 - 최빈수 구하기_2019.10.29
    https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV13zo1KAAACFAYh&categoryId=AV13zo1KAAACFAYh&categoryType=CODE&&&

---
+++ SWExpert/1204_bky373.java
@@ -0,0 +1,34 @@
+package study.swea;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class bky373_1204 {
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		int[] data = new int[1000];
+
+		for (int tc = 0; tc < T; tc++) {
+			int max = -1;
+			int mode = 0;
+			int[] freq = new int[101];
+			
+			int n = sc.nextInt();
+			for (int i = 0; i < 1000; i++) {
+				data[i] = sc.nextInt();
+				freq[data[i]]++;
+			}
+			for (int i = 0; i < freq.length; i++) {
+				if (max <= freq[i]) {
+					max = freq[i];
+					mode = i;
+				}
+			}
+			System.out.printf("#%d %d\n", n, mode);
+		}
+	}
+}
