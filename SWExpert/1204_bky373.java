9c4eb38d1f6466060c1a61c3a9507eef70ad2cbb
bky373 <49539592+bky373@users.noreply.github.com>
2019-10-29 13:31:36 +0900

    Update 1204_bky373.java

--- SWExpert/1204_bky373.java
+++ SWExpert/1204_bky373.java
@@ -1,9 +1,6 @@
 package study.swea;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Solution {
